84de9560b7810a4f9d65b954a0dd5020d7a8de85
Kul-RB <kulebyakin.roma@yandex.ru>
2023-10-03 15:12:48 +0000

    change rep

--- locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  platform = "${var.vpc_name}-${var.vm_name}"
-  db = "${var.vpc_name}-${var.vm_db_name}"
-}
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "db" {
-  value       = "${yandex_compute_instance.db.network_interface.0.nat_ip_address}"
-}
-
-output "platform"{
-  value       = "${yandex_compute_instance.platform.network_interface.0.nat_ip_address}"
-}
